d2fb941545f2c86e30c6058903d2ffa355eed826
edilsonlm217 <edilsonlm217@hotmail.com>
2019-10-19 22:21:50 -0400

    Finalização do Form

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,15 +1,36 @@
 import React from "react";
+
+import overflow from '../assets/overflow.png';
+
 import './Search.css'
 
 export default function Main() {
     return (
         <div className="container">
             <header>
-
+                <img src={overflow} alt='logo'/>
             </header>
             
             <main>
-
+            <form>
+                    <input
+                        id="tag" 
+                        placeholder="Informe uma tag" 
+                        required/>
+                    <input 
+                        id="score" 
+                        placeholder="Pontuação mínima" />
+                    <input 
+                        id="limit" 
+                        placeholder="Número de resultados"/>
+                    
+                    <select>
+                        <option value="" selected disabled hidden>Ordenar como</option>
+                        <option value="desc">Decrescenete</option>
+                        <option value="asc">Crescente</option>
+                    </select>
+                    <button>Pesquisar</button>
+                </form>
             </main>
             
             <footer>
