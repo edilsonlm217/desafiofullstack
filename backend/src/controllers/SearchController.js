a15a6a9a68659333a096c40be35a358eb5fb5d54
edilsonlm217 <edilsonlm217@hotmail.com>
2019-10-19 21:01:13 -0400

    Conectando o controlador 'SearchController' a API do StackOverflow

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -2,8 +2,10 @@ const axios = require('axios');
 
 module.exports = {
     async index(req, res) {
-        return res.json({
-            "message": "Hello World"
-        });
+        const { score, sort, limit, tag } = req.body;       
+        const response = await axios.get(`https://api.stackexchange.com/2.2/questions?pagesize=${limit}&order=${sort}&min=${score}&sort=votes&tagged=${tag}&site=stackoverflow`);
+        const { items } = response.data;
+        
+        return res.json(items);        
     }
 };
