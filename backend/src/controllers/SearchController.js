44430aa129a1b040161afb8bb3f1bae2cb71da70
edilsonlm217 <edilsonlm217@hotmail.com>
2019-10-19 21:07:32 -0400

    Formata o JSON para retornar apenas os dados que são uteis ao frontend

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -5,7 +5,17 @@ module.exports = {
         const { score, sort, limit, tag } = req.body;       
         const response = await axios.get(`https://api.stackexchange.com/2.2/questions?pagesize=${limit}&order=${sort}&min=${score}&sort=votes&tagged=${tag}&site=stackoverflow`);
         const { items } = response.data;
-        
-        return res.json(items);        
+        var items_array = items.map(function (array) {
+            return {
+                title: array.title,
+                link: array.link,
+                score: array.score,
+                owner: array.owner["display_name"],
+                profile_imageowner: array.owner["profile_image"],     
+                accept_rate: array.owner["accept_rate"],     
+                user_id: array.owner["user_id"],
+            } 
+        });
+        return res.json(items_array);        
     }
 };
